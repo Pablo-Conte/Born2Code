bb7f8b0332314429955121babb7a2e7d2cce11db
Pablo Conte Correa <pablo.correa.nr@gmail.com>
2022-08-31 00:52:02 -0300

    feat -> Errors catcher Done

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,6 +1,7 @@
 import { UserEntity } from "../database/entities/UserEntity";
 import { UsersRepository } from "../database/repositories/UsersRepository";
 import { hash } from "bcrypt";
+import { AppError } from "../shared/errors";
 
 type CreateUserDTO = {
     userData: UserEntity;
@@ -10,22 +11,28 @@ class CreateUserService{
 
     async execute({ userData }: CreateUserDTO): Promise<UserEntity>{
         
-        const { email, password } = userData;
+        const { email, password, birthDate } = userData;
         const usersRepository = new UsersRepository();
 
         const userConflict = await usersRepository.findByEmail({ email });
 
         if (userConflict) {
-            throw new Error("User Already exists!");
+            
+            throw new AppError("User Already exists!", 409);
         }
 
         const newPass = await hash(password, 10);
 
         userData.password = newPass;
 
+        if (userData?.birthDate){
+            userData.birthDate = new Date(userData.birthDate).toISOString() as unknown as Date;
+        }
+
+
         const newUser = await usersRepository.create({ uData: userData });
         if (!newUser) {
-            throw new Error ("User creation failed, contact support for more details")
+            throw new AppError("User creation failed, contact support for more details", 400)
         }
 
         return newUser;
--- src/shared/app.ts
+++ src/shared/app.ts
@@ -1,7 +1,9 @@
 import "dotenv/config";
+import "express-async-errors"
 import cors from "cors"; //Permite a comunicação entre o back e o front
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { router } from "./routes";
+import { AppError } from "./errors";
 
 const app = express();
 
@@ -9,6 +11,18 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true })) //isso vai passar por um middleware do próprio express onde vai comparar se os tipos que estão sendo passados são os mesmos tipos que queremos usar na aplicação.
 app.use(cors());
 app.use(router);
+
+app.use((err: Error, _request: Request, response: Response, _next: NextFunction) => {
+    if(err instanceof AppError){
+        return response.status(err.statusCode).json({
+            message: err.message
+        })
+    }
+    return response.status(500).json({
+        status: "Error",
+        message: `Internal Server Error, error: ${err}`
+    });
+});
 
 export { app }; 
 
